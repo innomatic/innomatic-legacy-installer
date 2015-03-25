7df23cae7fea6ab4a6a62667cf03c8d19dc6ea18
Alex Pagnoni <alex.pagnoni@innoteam.it>
2015-03-25 15:04:46 +0100

    Added missing license headers

--- src/Innomatic/Composer/LegacyApplicationInstaller.php
+++ src/Innomatic/Composer/LegacyApplicationInstaller.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * Innomatic
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ *
+ * @copyright  2015 Innomatic Company
+ * @license    http://www.innomatic.io/license/ New BSD License
+ * @link       http://www.innomatic.io
+ */
 namespace Innomatic\Composer;
 
 use Composer\Composer;
